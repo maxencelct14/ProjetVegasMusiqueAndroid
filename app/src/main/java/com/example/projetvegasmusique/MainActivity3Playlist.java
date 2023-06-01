8e7780e490bc80e24e9e89fbd7ce96710e20d800
Maxence Lecointre <maxencelecointre14@gmail.com>
2023-06-01 12:07:52 +0200

    commit 1 juin

--- app/src/main/java/com/example/projetvegasmusique/MainActivity3Playlist.java
+++ app/src/main/java/com/example/projetvegasmusique/MainActivity3Playlist.java
@@ -2,7 +2,10 @@ package com.example.projetvegasmusique;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3Playlist extends AppCompatActivity {
 
@@ -10,5 +13,40 @@ public class MainActivity3Playlist extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity3_playlist);
+        Button button1 = findViewById(R.id.buttoncreerplaylist);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3Playlist.this, CreerPlaylistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.buttonmodifierplaylist);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3Playlist.this, ModifierPlaylistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button3 = findViewById(R.id.buttonvoirplaylist);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3Playlist.this, VoirPlaylistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button4 = findViewById(R.id.buttonvoirplaylists);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3Playlist.this, VoirPlaylistsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
